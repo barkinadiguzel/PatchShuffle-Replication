60b7eb6be9d77659b291557232ee3dda48176eb1
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-26 23:32:38 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Input / dataset
+IMG_SIZE = (32, 32)       
+NUM_CLASSES = 10          
+BATCH_SIZE = 16            
+
+# PatchShuffle parameters
+PATCH_SIZE = (2, 2)       
+SHUFFLE_PROB = 0.1         # ε, patch shuffle prob
+
+# Model parameters
+FC_IN_FEATURES = 128*4*4  
